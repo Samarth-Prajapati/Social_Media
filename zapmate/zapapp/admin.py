fae0da18ec5342bd4d15a71ec076fbedb2973751
Samarth Prajapati <samarthp475@gmail.com>
2024-08-30 15:40:59 +0530

    updated

--- zapmate/zapapp/admin.py
+++ zapmate/zapapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Like)
+admin.site.register(Follows)
+admin.site.register(TimeCapsule)
+admin.site.register(ZapTriggers)
+admin.site.register(Profile)
+admin.site.register(Comment)
